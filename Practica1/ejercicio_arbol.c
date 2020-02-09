8e4802409b4ad1d1978d6e9c41d4c2a41085d9b8
laurapcc <lauradepazc@gmail.com>
2020-02-09 13:15:27 +0100

    Ejercicio 8 (creo que esta bien)

--- Practica1/ejercicio_arbol.c
+++ Practica1/ejercicio_arbol.c
@@ -3,24 +3,29 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdint.h>
+
 
 #define NUM_PROC 3
 
 int main(void){
   pid_t pid;
-	pid = fork();
-  for (int i=0; i<NUM_PROC; i++){
+	
+  for (int i=0; i < NUM_PROC; i++){
+		pid = fork();
   	if(pid <  0){
   		perror("fork");
   		exit(EXIT_FAILURE);
   	}
   	else if(pid > 0){
-      fprintf(stdout, "j%d", (intmax_t)getpid())
-      wait(NULL);
-  		exit(EXIT_SUCCESS);
+      fprintf(stdout, "PID = %jd\n", (intmax_t)getpid());
+			wait(NULL);
+   		exit(EXIT_SUCCESS);
   	}
+		else if (pid == 0){
+			fprintf(stdout, "PID = %jd   PPID = %jd\n",(intmax_t)getpid(), (intmax_t)getppid());
+		}
   }
-  sleep(200);
 	wait(NULL);
 	exit(EXIT_SUCCESS);
 }
