b923e6fefa5e0f817c4ee43d4da93b49a8ef94f3
jorgedemi <55850524+jorgedemi@users.noreply.github.com>
2020-02-14 17:55:34 +0100

    shell bien

--- Practica1/ejercicio_shell.c
+++ Practica1/ejercicio_shell.c
@@ -10,30 +10,35 @@
 
 int main(){
   pid_t pid;
-  char *buffer;
+  char *buffer=NULL;
   size_t num_bytes;
   wordexp_t command;
   int status;
 
-
-  
   while(getline(&buffer, &num_bytes, stdin) != EOF){
+    buffer[strlen(buffer)-1] = '\0';
     pid = fork();
+
     if (pid < 0) return EXIT_FAILURE;
+
     else if (pid > 0){ /* padre */
       wait(&status);
-      perror("Exited with value");
+      if (WIFEXITED(status)){
+        fprintf(stderr, "Exited with value %d\n", WEXITSTATUS(status));
+      }
+      fprintf(stderr, "Terminated by signal %d\n", WTERMSIG(status));
+
     }
+
     else{ /* hijo */
       if (wordexp(buffer, &command, 0) != 0){
-        wordfree(&command);
         return EXIT_FAILURE;
       }
-      execv(command.we_wordv[0], command.we_wordv);
+      execvp(command.we_wordv[0], command.we_wordv);
       wordfree(&command);
     }
+    fprintf(stdout, "\n");
   }
-  
 
   return EXIT_SUCCESS;
 }
