87722cc96f13050cc7831939584d5c550caa4df9
Laura de Paz <laura_depaz@hotmail.com>
2020-04-06 20:16:24 +0200

    ej3 a mitad

--- Practica3/shm_concurrence.c
+++ Practica3/shm_concurrence.c
@@ -48,8 +48,12 @@ void manejador (int sig) {
 }
 
 int main(int argc, char *argv[]) {
-	int n, m;
+	int n, m, i;
 	int ret = EXIT_FAILURE;
+	pid_t pid;
+	pid_t array_pid[n];
+	struct sigaction act;
+    sigset_t set;
 
 	if (argc < 3) {
 		fprintf(stderr,"usage: %s <n_process> <n_logs> \n",argv[0]);
@@ -59,7 +63,85 @@ int main(int argc, char *argv[]) {
 	n = atoi(argv[1]);
 	m = atoi(argv[2]);
 
-	/* ... */
+	/* crear n hijos */
+	for (i = 0; i < n; i++){
+        pid = fork();
+        if (pid < 0){
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid){ /* padre */
+            array_pid[i] = pid;
+        }
+        else{ /* hijo */
+            break;
+        }
+    }
 
-	return ret;
+	if (pid){ /* padre */
+		/* crear memoria compartida */
+		int fd_shm = shm_open(SHM_NAME,
+        			O_RDWR | O_CREAT | O_EXCL, 
+        			S_IRUSR | S_IWUSR);
+
+		if (fd_shm == -1) {
+			fprintf(stderr, "Error creating the shared memory segment\n");
+			return EXIT_FAILURE;
+    	}
+
+		/* Truncar el tamano de la memoria compartida */
+		if (ftruncate(fd_shm, sizeof(ClientLog)) == -1) {
+			fprintf(stderr, "Error resizing the shared memory segment\n");
+			shm_unlink(SHM_NAME);
+			return EXIT_FAILURE;
+		}
+
+		/* manejar SIGUSR1 */
+        sigemptyset(&(act.sa_mask));
+        act.sa_flags = 0;
+        act.sa_handler = manejador;
+        if (sigaction(SIGUSR1, &act, NULL) < 0) {
+            fprintf(stderr, "Error in sigaction\n");
+			shm_unlink(SHM_NAME);
+            return ret;
+        }
+
+        /* suspender proceso hasta que reciba SIGUSR1 */
+        sigfillset(&set);
+        sigdelset(&set, SIGUSR1);
+        sigsuspend(&set);
+		
+		/**************************
+		
+		CREO QUE FALTA ALGO POR AHI ARRIBA DEL PADRE PARA QUE PUEDA LEER
+		QUIZAS UN MMAP, NO SE
+		 
+		**************************/
+
+		/* espera a que acaben todo los hijos */
+		for (i = 0; i < n; i++)
+			wait(NULL);
+		
+		return EXIT_SUCCESS;
+	}
+	
+	//else
+	for (i = 0; i < m; i++){
+		usleep((rand()%801 + 100) * 1000); /* random entre 100 y 900 ms */
+		/*
+		Rellenará la información de la estructura ClientLog para generar una
+		nueva lı́nea de log, de manera que processid sea igual al PID del
+		proceso, se incremente el campo logid (clinealogid = clinealogid + 1),
+		y logtext contenga el mensaje "Soy el proceso <PID> a las HH:MM:SS:mmm"
+		*/
+
+		/* enviar senal SIGUSR1 a padre */
+		if (kill(getppid(), SIGUSR1) < 0){
+			fprintf(stderr, "Error en proceso con pid = %jd al enviar SIGUSR1\n", (intmax_t)getpid());
+			/* liberar shm y demas */
+			return ret;
+		}
+	}
+
+	return EXIT_SUCCESS;
 }
