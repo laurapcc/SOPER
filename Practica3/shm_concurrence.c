74597aef8320ce4ddb4a0382eaa6917ed64d76c6
Laura de Paz <laura_depaz@hotmail.com>
2020-04-08 11:26:49 +0200

    ej3, en ello estamos

--- Practica3/shm_concurrence.c
+++ Practica3/shm_concurrence.c
@@ -4,6 +4,7 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
@@ -42,9 +43,11 @@ typedef struct {
 ClientLog *shm_struct;
 
 void manejador (int sig) {
+	printf("   padre en manejador!!!\n");
 	if (sig == SIGUSR1) {
 		printf ("Log %ld: Pid %d: %s\n",shm_struct->logid, shm_struct->processid, shm_struct->logtext);
 	}
+	printf("   padre sale de manejador...\n");
 }
 
 int main(int argc, char *argv[]) {
@@ -79,6 +82,8 @@ int main(int argc, char *argv[]) {
     }
 
 	if (pid){ /* padre */
+		printf("padre\n");
+
 		/* crear memoria compartida */
 		int fd_shm = shm_open(SHM_NAME,
         			O_RDWR | O_CREAT | O_EXCL, 
@@ -96,6 +101,20 @@ int main(int argc, char *argv[]) {
 			return EXIT_FAILURE;
 		}
 
+		/* Mapear el segmento de memoria */
+		ClientLog *my_struct = mmap(NULL, sizeof(ClientLog), PROT_READ | PROT_WRITE,
+									MAP_SHARED, fd_shm, 0);
+		close(fd_shm);
+
+		if (my_struct == MAP_FAILED) {
+			fprintf(stderr, "Error mapping the shared memory segment\n");
+			shm_unlink(SHM_NAME);
+			return ret;
+		}
+
+		/* Inicializar logid a -1 */ /* NO SE ESTO ESTA BIEN AQUI */
+		my_struct->logid = -1;
+
 		/* manejar SIGUSR1 */
         sigemptyset(&(act.sa_mask));
         act.sa_flags = 0;
@@ -110,24 +129,41 @@ int main(int argc, char *argv[]) {
         sigfillset(&set);
         sigdelset(&set, SIGUSR1);
         sigsuspend(&set);
-		
-		/**************************
-		
-		CREO QUE FALTA ALGO POR AHI ARRIBA DEL PADRE PARA QUE PUEDA LEER
-		QUIZAS UN MMAP, NO SE
-		 
-		**************************/
 
 		/* espera a que acaben todo los hijos */
 		for (i = 0; i < n; i++)
 			wait(NULL);
+
+		munmap(my_struct, sizeof(ClientLog));
+    	shm_unlink(SHM_NAME);
 		
 		return EXIT_SUCCESS;
 	}
-	
 	//else
+
+	printf("hijo\n");
+
 	for (i = 0; i < m; i++){
 		usleep((rand()%801 + 100) * 1000); /* random entre 100 y 900 ms */
+
+		/* abrir la memoria compartida */
+		int fd_shm = shm_open(SHM_NAME, O_RDWR, 0); 
+
+		if (fd_shm == -1) {
+			fprintf(stderr, "Error opening the shared memory segment (pid = %jd)\n", (intmax_t)getpid());
+			perror("open memory");
+			return EXIT_FAILURE;
+		}
+
+		/* Mapear el segmento de memoria */
+		ClientLog *my_struct = mmap(NULL, sizeof(ClientLog), PROT_WRITE, MAP_SHARED, fd_shm, 0);
+		close(fd_shm);
+
+		if (my_struct == MAP_FAILED) {
+			fprintf(stderr, "Error mapping the shared memory segment\n");
+			return EXIT_FAILURE;
+		}
+
 		/*
 		Rellenará la información de la estructura ClientLog para generar una
 		nueva lı́nea de log, de manera que processid sea igual al PID del
@@ -135,13 +171,29 @@ int main(int argc, char *argv[]) {
 		y logtext contenga el mensaje "Soy el proceso <PID> a las HH:MM:SS:mmm"
 		*/
 
+
+		/* Escribir en la estructura compartida */
+		printf("A escibir (pid = %jd)\n", (intmax_t)getpid());
+		my_struct->processid = getpid();
+		my_struct->logid++;
+		char buf[MAX_MSG]; 
+		getMilClock(buf);
+		memcpy(my_struct->logtext, buf, MAX_MSG);
+		printf("Termina escibir (pid = %jd)\n", (intmax_t)getpid());
+
+
 		/* enviar senal SIGUSR1 a padre */
 		if (kill(getppid(), SIGUSR1) < 0){
 			fprintf(stderr, "Error en proceso con pid = %jd al enviar SIGUSR1\n", (intmax_t)getpid());
-			/* liberar shm y demas */
+			/* liberar algo de shm? en el ejmplo de reader no lo hace */
 			return ret;
 		}
+
+		/* Unmap the shared memory */
+		munmap(my_struct, sizeof(ClientLog));
 	}
+	printf("hijo termina guay\n");
+
 
 	return EXIT_SUCCESS;
 }
